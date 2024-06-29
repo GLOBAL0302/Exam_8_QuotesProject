fa82719184ef8ecd9461e412652aa8bbc75145d9
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-06-29 13:37:30 +0400

    divided home into two mainComponents sideBar and quotes

--- src/components/Quotes/Quotes.tsx
+++ src/components/Quotes/Quotes.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import {IQuote} from '../../types';
+import React, {useCallback, useEffect, useState} from 'react';
+import {IApiQuotes, IQuote} from '../../types';
 import Quote from './Quote/Quote';
+import axiosApi from '../../axiosApi';
 
-interface Props{
-  quotes:IQuote[]
-}
-const Quotes:React.FC<Props> = ({quotes}) => {
+// interface Props{
+//   link:string
+// }
+const Quotes= () => {
+  const [allQuotes, setAllQuotes] = useState<IQuote[]>([
+  ]);
+
+  const fetchQuoteData = useCallback(async ()=>{
+    const response =await axiosApi.get<IApiQuotes | null>("/quotes.json");
+    const quotesResponse = response.data;
+    if(quotesResponse){
+      const quotes = Object.keys(response.data).map((id:string)=>({
+        ...response.data[id],
+        id,
+      }));
+
+      setAllQuotes(quotes);
+    }
+  },[]);
+
+  useEffect(() => {
+    void  fetchQuoteData();
+  }, [fetchQuoteData]);
+
+
+
   return (
     <div className="col-8">
-      {quotes.map((quote)=>(
+      {allQuotes.map((quote)=>(
         <Quote
           key={quote.id}
           quote={quote}/>
--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -1,36 +1,14 @@
-import {useCallback, useEffect, useState} from 'react';
-import {IApiQuotes, ICategories, IQuote, IUserInput} from '../../types';
-import axiosApi from '../../axiosApi';
 import Quotes from '../../components/Quotes/Quotes';
 import Category from '../../components/Category/Category';
 
 
 
 const HomePage = () => {
-  const [allQuotes, setAllQuotes] = useState<IQuote[]>([
-  ]);
-
-  const fetchQuoteData = useCallback(async ()=>{
-    const response =await axiosApi.get<IApiQuotes | null>("/quotes.json");
-    const quotesResponse = response.data;
-    if(quotesResponse){
-      const quotes = Object.keys(response.data).map((id:string)=>({
-        ...response.data[id],
-        id,
-      }));
-
-      setAllQuotes(quotes);
-    }
-  },[]);
-
-  useEffect(() => {
-    void  fetchQuoteData();
-  }, [fetchQuoteData]);
 
   return (
     <div className="d-flex gap-5">
       <Category/>
-      <Quotes quotes = {allQuotes}/>
+      <Quotes/>
     </div>
   );
 };
