847b89327d7dafff48b8f6fa39ac65d557c19c76
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-06-29 10:09:41 +0400

    added navBar and installed axios lib

---
+++ src/axiosApi.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const AxiosApi = axios.create({
+  baseURL: "https://kubanychjs25-default-rtdb.europe-west1.firebasedatabase.app/"
+});
+
+export default AxiosApi
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,21 @@
 const NavBar = () => {
   return (
-    <div>
-      <p>Nav</p>
-    </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Home</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav d-flex ms-auto gap-3">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">Quotes</a>
+            </li>
+           <span style={{height:"40px", width:"3px"}} className="border border-2 bg-light"></span>
+            <li className="nav-item">
+              <a className="nav-link " aria-current="page" href="#">Submit new Quotes</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
